32636ef3577b6926cf16b0c6a1bc55264d69c2ab
Usrani <usrani712@gmail.com>
2024-06-26 13:39:34 +0530

    changes

---
+++ Estore.Web/Controllers/OrderHeaderDto.cs
@@ -0,0 +1,9 @@
+﻿//using EStore.Web.DTO;
+//using Microsoft.AspNetCore.Mvc;
+//using EStore.Web.DTO;
+//using EStore.Web.Models.Dto;
+internal class OrderHeaderDto
+{
+    internal readonly object OrderHeaderId;
+    internal readonly string Status;
+}
